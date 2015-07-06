5df58cb2afb331036ccfcbfa3914ab7e32e8792e
James Dabbs <jamesdabbs@gmail.com>
2015-07-06 10:06:27 -0400

    Require login before posting items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     Item.all
+  end
+
+  def new
+    @item = Item.new seller_id: current_user.id
+    #@item = current_user.items.new
+  end
+
+  def create
   end
 end
