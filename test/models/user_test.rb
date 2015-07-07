a02977e5de28c01d1402cec46cb88b4930baf0a1
James Dabbs <jamesdabbs@gmail.com>
2015-07-07 10:04:53 -0400

    Add model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_user_knows_sold_items
+    u = User.first
+    assert_equal 0, u.sold_items.count
+
+    u.sold_items << Item.first
+    assert_equal 1, u.sold_items.count
+  end
 end
