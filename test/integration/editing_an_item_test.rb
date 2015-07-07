73d4a84e3ea617f269e8b238142cbe4540a54471
James Dabbs <jamesdabbs@gmail.com>
2015-07-07 11:51:43 -0400

    Add integration test for form validation and redisplay

--- test/integration/editing_an_item_test.rb
+++ test/integration/editing_an_item_test.rb
@@ -1,25 +1,40 @@
 require 'test_helper'
 
 class EditingAnItemTest < ActionDispatch::IntegrationTest
-  def test_happy_path
+  def setup
     # Sign in
-    u = User.create! email: "user@example.com", password: "password"
+    @u = User.create! email: "user@example.com", password: "password"
     visit new_user_session_path
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     click_button "Log in"
 
     click_link "New item"
+  end
 
+  def test_happy_path
     within "#new_item" do
       fill_in "Name", with: "Concrete Granite Cat Things"
       fill_in "Price", with: 9001
       click_button "Save"
     end
 
-    # assert_equal 9001, Item.last.price ?
+    # Should this be here?
+    assert_equal 9001, @u.sold_items.last.price
 
     assert_includes find('h1').text, "Concrete Granite Cat Things"
     assert_includes find('h2').text, "9001.0"
+  end
+
+  def test_validation_redisplay
+    within "#new_item" do
+      fill_in "Name", with: "Concrete Granite Cat Things"
+      click_button "Save"
+    end
+
+    # assert page.has_css? "help-block"
+    assert_includes find(".help-block").text, "can't be blank"
+    refute page.has_css? "h1"
+    assert page.has_css? ".has-error #item_price"
   end
 end
