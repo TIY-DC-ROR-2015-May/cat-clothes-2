58080976898a45a601cc2d071625544211b653c3
James Dabbs <jamesdabbs@gmail.com>
2015-07-08 11:42:42 -0400

    Wire up buy button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: :omniauth_callbacks }
 
-  resources :items
+  resources :items # do
+  #   member do
+  #     post :buy
+  #   end
+  # end
+
+  resource :cart, only: [:update]
 
   root to: 'items#index'
 end
