1bf16b7341185e7dff6fe268ef635b83a43e444b
Kathryn Walsh <kwals@user.noreply.github.com>
2015-07-15 14:23:41 -0400

    Invoices calculate total cost: g

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,13 +12,11 @@ class Cart
   end
 
   def self.create_invoice(items_ids)
-    @invoice = Invoice.create!
+    @invoice = Invoice.create!(total_cost: 0)
     items_ids.each do |item_id|
       item = Item.find(item_id)
       @invoice.invoice_items.create(item_id: item.id, quantity: 1)
-      @invoice.total_cost ||= 0
-      @invoice.total_cost += item.price.to_i
-      
+      @invoice.update(total_cost: (@invoice.total_cost += item.price.to_i))
     end
     return @invoice 
   end
